63034aa938fd54c29498c2329637b7bb11808b3b
Emile-Nsengimana <emilereas7@gmail.com>
2019-09-25 16:23:35 +0200

    bg(signup-redirect): redirect after user signup
      ensures that users get redirected after signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import LandingPage from './views/LangingPage';
 import Login from './views/Login';
 import Signup from './views/Signup';
 import HomePage from './views/HomePage';
-import Transactions from './views/Transactions';
+
+const GoogleVerify = () => (
+  <>
+    google-site-verification: googled22c070829fdf48c.html
+  </>
+);
 
 const App = () => (
   <Router>
     <Switch>
+      <Route exact path="/googled22c070829fdf48c" component={GoogleVerify} />
       <Route exact path="/" component={LandingPage} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/home" component={HomePage} />
-      <Route exact path="/transaction" component={Transactions} />
     </Switch>
   </Router>
 );
